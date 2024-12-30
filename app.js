e86df260ac18b9070304732f62a76e659476d633
DenisBisekeev <bisekeevdenis@mail.ru>
2024-12-30 16:49:47 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+var text = document.getElementById('out');
+
+function clear() {
+    text.innerHTML = 'Тут появится текст...';
+}
+function black() {
+    text.innerHTML = '"Чёрный" - NEX - #000000, RGB - (0, 0, 0)';
+} 
+function grey() {
+    text.innerHTML = '"Серый" - NEX - #808080, RGB - (128, 128, 128)';
+}
+function silver() {
+    text.innerHTML = '"Серебристый" - NEX - #C0C0C0, RGB - (192, 192, 192)'
+}
+function white() {
+    text.innerHTML = '"Белый" - NEX - #FFFFFF, RGB - (255, 255, 255)'
+}
+function pink() {
+    text.innerHTML = '"Розовый" - NEX - #FF00FF, RGB - (255, 0, 255)'
+}
+function purple() {
+    text.innerHTML = '"Фиолетовый" - NEX - #800080, RGB - (128, 0, 128)'
+}
+function red() {
+    text.innerHTML = '"Красный" - NEX - #FF0000, RGB - (255, 0, 0)'
+}
+function blackred(){
+    text.innerHTML = '"Тёмно-красный" - NEX - #800000, RGB - (128, 0, 0)'
+}
+function yellow() {
+    text.innerHTML = '"Жёлтый" - NEX - #FFFF00, RGB - (255, 255, 0)'
+}
+function olive() {
+    text.innerHTML = '"Оливковый" - NEX - , RGB - (128, 128, 0)'
+}
+function lime() {
+    text.innerHTML = '"Лаймовый" - NEX - #00FF00, RGB - (0, 255, 0)'
+}
+function green() {
+    text.innerHTML = '"Зелёный" -  NEX - #008000, RGB - (0, 128, 0)'
+}
+function aqua() {
+    text.innerHTML = '"Аква" - NEX - #00FFFF, RGB - (0, 255, 255)'
+}
+function teal() {
+    text.innerHTML = '"Бирюзовый" - NEX - #008080, RGB - (0, 128, 128)'
+}
+function blue() {
+    text.innerHTML = '"Синий" - NEX - #0000FF, RGB - (0, 0, 255)'
+}
+function blackblue() {
+    text.innerHTML = '"Тёмно-синий" - NEX - #000080, RGB - (0, 0, 128)'
+}
+function brown() {
+    text.innerHTML = '"Коричневый" - NEX - #964B00, RGB - (150,75,0)'
+}
